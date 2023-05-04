0a06c468292439e59e3b0c8736d762dc11515219
NatSison <111641301+NatSison@users.noreply.github.com>
2023-05-04 10:04:52 +0800

    Delete System Project directory

--- System Project/app/helpers/url_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Simple page redirect
-    function redirect($page){
-        header('location: ' . URLROOT . '/' . $page);
-    }
-	
-	function redirectCurrent(){
-		header('location: '. REFERER);
-	}
